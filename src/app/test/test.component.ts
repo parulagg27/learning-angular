e9c7ce94104b2dcbf947a195d349629ccc804e5f
Parul <parulagg27@gmail.com>
2021-01-04 02:13:06 +0530

    Learnt two-way binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: '[app-test]',
   template: `
     <h2>Welcome to {{name}}</h2>
-    <input type="text" #userInput>
-    <button (click)="onClick(userInput.value)">Submit</button>
+    <input type="text" [(ngModel)]="inputValue">
+    {{inputValue}}
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  name = "Codevolution";
+  name = 'Codevolution';
+  inputValue = '';
 
-  constructor() { }
-
-  onClick(value){
-    console.log(value);
+  constructor() {
   }
 
   ngOnInit(): void {
