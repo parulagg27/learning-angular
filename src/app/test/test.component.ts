3b0459fbe612e4428223824dde8bf5a32058a65d
Parul <parulagg27@gmail.com>
2021-01-03 22:36:21 +0530

    [Parul] Add. learning Event Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,23 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-    <h2 [style.color]="'orange'"> Style Binding</h2>
-    <h2 [style.color]="highlightColor"> Style Binding 2</h2>
-    <h2 [style.color]="hasError ? 'red' : 'green'">Style Binding 3</h2>
-    <h2 [ngStyle]="multiCSS">Style Binding 3</h2>
+    <h2>Welcome to {{name}}</h2>
+
+    <button (click)="onClick()">Event Binding with Event Handler</button>
+
+    <button (click)="greeting='Welcome Parul'">Greet User on Page</button>
+    {{greeting}}
+
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  highlightColor = "green";
-  hasError = true;
-
-  multiCSS = {
-    "color": "purple",
-    "font-style": "italic"
-  };
+  name = "Codevolution";
+  greeting = "";
 
   constructor() { }
+
+  onClick(){
+    console.log("Welcome to Angular Tutorial")
+  }
 
   ngOnInit(): void {
   }
