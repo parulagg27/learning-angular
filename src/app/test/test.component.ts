df5774129bd004e1fba197d0f1face0d4e5e5fba
Parul <parulagg27@gmail.com>
2021-01-04 02:50:48 +0530

    Learnt structural directive: ngIF

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,15 +3,25 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `
-    <h2>Welcome to {{name}}</h2>
-    <input type="text" [(ngModel)]="inputValue">
-    {{inputValue}}
+    <!--    <h1 *ngIf="toBeDisplayed; else elseBlock">Codevolution</h1>-->
+
+    <!--    <ng-template #elseBlock>-->
+    <!--      <h2>Hidden first Element</h2>-->
+    <!--    </ng-template>-->
+    <div *ngIf="toBeDisplayed; then thenBlock; else elseBlock"></div>
+
+    <ng-template #thenBlock>
+      <h2>First Block</h2>
+    </ng-template>
+    <ng-template #elseBlock>
+      <h2>Second Block</h2>
+    </ng-template>
+
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  name = 'Codevolution';
-  inputValue = '';
+  toBeDisplayed = true;
 
   constructor() {
   }
