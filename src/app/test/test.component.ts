55312752e10ddd36f9f7ec4d616baadebd40baf2
Parul <parulagg27@gmail.com>
2021-01-05 22:33:15 +0530

    [Parul] Add. learn component interaction: parent to child (via data binding)

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,21 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
+  selector: 'app-test',
   template: `
-    <div *ngFor="let colour of colors; index as i; first as f; last as l, even as e, odd as o">
-      <p>
-        {{i}}. {{colour}}, First: {{f}}, Last: {{l}},
-        Even: {{e}},
-        Odd: {{o}}
-      </p>
-    </div>
+    <h2>{{"Welcome, " + username}}</h2>
   `,
 })
 export class TestComponent implements OnInit {
-  colors: string[] = ["red", "orange", "blue"];
+  @Input('name') username: string;
 
   ngOnInit(): void {
   }
 
 }
+
