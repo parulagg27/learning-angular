2e608715241779aa4fe25cbef47a79672d86867e
Parul <parulagg27@gmail.com>
2021-01-05 22:34:37 +0530

    [Parul] Add. learn component interaction: child to parent (via custom event binding)

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -4,12 +4,18 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
   selector: 'app-test',
   template: `
     <h2>{{"Welcome, " + username}}</h2>
+    <button (click)="fireEvent()">Send Event</button>
   `,
 })
 export class TestComponent implements OnInit {
   @Input('name') username: string;
+  @Output() childEvent: EventEmitter<any> = new EventEmitter();
 
   ngOnInit(): void {
+  }
+
+  fireEvent(){
+    this.childEvent.emit("Welcome to Codevolution!");
   }
 
 }
